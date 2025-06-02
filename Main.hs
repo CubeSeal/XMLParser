1d3b2a68da7bd9ed2c27c91587093da8e58a19f1
landseal <kenndesilva1@gmail.com>
2025-06-02 17:06:27 +1000

    WIP: Doing Something.

--- Main.hs
+++ Main.hs
@@ -1,11 +1,11 @@
 import Control.Applicative ((<|>), Alternative)
 import GHC.Base (Alternative(..))
 import Debug.Trace (trace, traceShow)
--- XML Data Structure
 
+-- XML Data Structure
 newtype Attribute = Attribute [String]
   deriving Show
-newtype Tag = Tag (String, Attribute)
+newtype Tag = Tag {showTag :: (String, Attribute)}
   deriving Show
 data XML = XML (Tag, [XML]) | Literal String
   deriving Show
@@ -37,47 +37,47 @@ strPred :: (Char -> Bool) -> Parser String
 strPred p = Parser f
   where
     f [] = Nothing
-    f input = g $ span p input
-    g f' = case f' of
-      ("", _) -> Nothing
-      _ -> Just f'
+    f input = catch $ span p input
+    catch ("", _) = Nothing
+    catch tpl = Just tpl
 
 charP :: Char -> Parser Char
-charP x = Parser f
+charP p = Parser f
   where
-    f (c:str) = if c == x then Just (c, str) else Nothing
+    f (x:xs)
+      | x == p = Just (x, xs)
+      | otherwise = Nothing
     f [] = Nothing
 
 strP :: String -> Parser String
 strP = traverse charP
 
 parseOpeningTag :: Parser Tag
-parseOpeningTag = charP '<' *> f <* charP '>'
+parseOpeningTag = charP '<' *> Parser g <* charP '>'
   where
-    f = Parser $ \(c:str) -> if c == '/' then Nothing else do
-      (tagInfo, input') <- runParser (strPred (/= '>')) (c:str)
+    g ('/':xs) = Nothing
+    g xs = do
+      (tagInfo, input') <- runParser (strPred (/= '>')) xs
       let ls = words tagInfo
           tag = head ls
           attributes = Attribute $ tail ls
       Just (Tag (tag, attributes), input')
 
+parseClosingTag :: Tag -> Parser String
+parseClosingTag (Tag (str, _)) = charP '<' *> charP '/' *> strP str <* charP '>'
+
 singleFieldParser :: Parser XML
 singleFieldParser = Parser $ \input -> do
   (tag, input') <- runParser parseOpeningTag input
-  let
-    tagStr = case tag of
-      Tag (str, _) -> str
-    parseClosingTag = charP '<' *> charP '/' *> strP tagStr <* charP '>'
   (xml, input'') <- runParser xmlParser input'
-  (_, input''') <- runParser parseClosingTag input''
+  (_, input''') <- runParser (parseClosingTag tag) input''
   Just (XML (tag, xml), input''')
 
 literalParser :: Parser XML
 literalParser = Parser f
   where
     f "" = Nothing
-    f input = runParser g input
-    g = Literal <$> strPred (`notElem` ['<', '>'])
+    f input = flip runParser input $ Literal <$> strPred (`notElem` ['<', '>'])
 
 xmlParser :: Parser [XML]
 xmlParser = many (singleFieldParser <|> literalParser)
